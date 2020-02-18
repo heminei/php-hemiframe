f8d7e10238fcc20ee69778b512ef17f3860e2cf8
heminei <hemineibg1@gmail.com>
2020-02-19 00:26:59 +0200

    fixed bug in router class

--- src/HemiFrame/Lib/Router.php
+++ src/HemiFrame/Lib/Router.php
@@ -465,7 +465,7 @@ class Router
         }
 
         foreach ($this->urlArray as $key => $urlPreg) {
-            $urlPreg = preg_replace("/\{\{(.*?)\}\}/i", "(?<$1>[a-zA-Zа-яА-Я0-9абвгдежзийклмнопрстуфхцчшщъьюя=\.@_:\[\]\-\s\%\+]+)", $urlPreg);
+            $urlPreg = preg_replace("/\{\{(.*?)\}\}/i", "(?<$1>[a-zA-Zа-яА-Я0-9абвгдежзийклмнопрстуфхцчшщъьюя=\.@_:\[\]\-\s\%\+'\",]+)", $urlPreg);
             $urlPreg = str_replace("/", "\/", $urlPreg);
 
             $matches = [];
